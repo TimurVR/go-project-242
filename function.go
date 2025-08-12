ccaddd1dafd3d91d7c682f8be622a57daff2ddaa
TimurVR <138249026+TimurVR@users.noreply.github.com>
2025-08-12 18:33:48 +0300

    The fourth commit

--- function.go
+++ function.go
@@ -1,30 +1,63 @@
 package goproject242
-import ("os"
-"path/filepath"
-"io/fs"
-"strconv")
-
-func GetPathSize(path string, recursive, human, all bool) (string, error){
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"io/fs"
+)
+func GetPathSize(path string, recursive, human, all bool) (string, error) {
 	fileInfo, err := os.Lstat(path)
-
+	if err != nil {
+		return "", err
+	}
 	var totalSize int64
 	if fileInfo.Mode().IsRegular(){
-		totalSize=fileInfo.Size()
-	}else{	
+		totalSize = fileInfo.Size()
+	} else {
+		if all{
 		err = filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if !d.IsDir() {
-			info, err := d.Info()
 			if err != nil {
 				return err
 			}
-			totalSize += info.Size()
+			if d.IsDir()==false  {
+				info, err := d.Info()
+				if err != nil {
+					return err
+				}
+				totalSize += info.Size()
+			}
+			
+			return nil
+		})
+		}else{
+			entries, err := os.ReadDir(path)
+			if err!=nil{
+				return "",err
+			}
+			for _, entry := range entries {
+				path1 := filepath.Join(path, entry.Name())
+				fileInfo1, err := os.Lstat(path1)
+				if err != nil {
+					return "", err
+				}
+				totalSize += fileInfo1.Size()
+			}
 		}
-		return nil
-	})
 	}
-	s:=strconv.FormatInt(totalSize,10)
-	return s+"\t"+path,err
+	if human {
+		return fmt.Sprintf("%s\t%s", formatHumanReadable(totalSize), path), err
+	}
+	return fmt.Sprintf("%dB\t%s", totalSize, path), err
+}
+func formatHumanReadable(size int64) string {
+	const unit = 1024
+	if size < unit {
+		return fmt.Sprintf("%dB", size)
+	}
+	div, exp := int64(unit), 0
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f%cB", float64(size)/float64(div), "KMGTPE"[exp])
 }
---
+++ testdata/path_size_test.go
@@ -0,0 +1,70 @@
+package testdata_test
+
+import (
+    "testing"
+    code1 "code"
+    "github.com/stretchr/testify/require"
+)
+
+func TestGetPathSizeHumanTxt(t *testing.T) {
+    ex1, err1 := code1.GetPathSize("example.txt", true, true, true)
+    if err1 != nil {
+        t.Errorf(`Error in testing of getting path size %v`, err1)
+    }
+    require.Equal(t, "45B\texample.txt", ex1, "Expected format: size\\tfilename")
+}
+
+func TestGetPathSizeNotHumanTxt(t *testing.T) {
+    ex1, err1 := code1.GetPathSize("example.txt", true, false, true)
+    if err1 != nil {
+        t.Errorf(`Error in testing of getting path size %v`, err1)
+    }
+    require.Equal(t, "45B\texample.txt", ex1, "Expected format: size\\tfilename")
+}
+
+func TestGetPathSizeHumanDocx(t *testing.T) {
+    ex1, err1 := code1.GetPathSize("example.docx", true, true, true)
+    if err1 != nil {
+        t.Errorf(`Error in testing of getting path size %v`, err1)
+    }
+    require.Equal(t, "16.5KB\texample.docx", ex1, "Expected format: size\\tfilename")
+}
+
+func TestGetPathSizeNotHumanDocx(t *testing.T) {
+    ex1, err1 := code1.GetPathSize("example.docx", true, false, true)
+    if err1 != nil {
+        t.Errorf(`Error in testing of getting path size %v`, err1)
+    }
+    require.Equal(t, "16902B\texample.docx", ex1, "Expected format: size\\tfilename")
+}
+func TestGetPathSizeHumanDirOnlyFiles(t *testing.T) {//r
+    ex1, err1 := code1.GetPathSize("example", true, true, false)
+    if err1 != nil {
+        t.Errorf(`Error in testing of getting path size %v`, err1)
+    }
+    require.Equal(t, "18.7KB\texample", ex1, "Expected format: size\\tfilename")
+}
+
+func TestGetPathSizeNotHumanDirOnlyFiles(t *testing.T) {//r
+    ex1, err1 := code1.GetPathSize("example", true, false, false)
+    if err1 != nil {
+        t.Errorf(`Error in testing of getting path size %v`, err1)
+    }
+    require.Equal(t, "19131B\texample", ex1, "Expected format: size\\tfilename")
+}
+
+func TestGetPathSizeHumanDirAll(t *testing.T) {//r
+    ex1, err1 := code1.GetPathSize("example", true, true, true)
+    if err1 != nil {
+        t.Errorf(`Error in testing of getting path size %v`, err1)
+    }
+    require.Equal(t, "19.2KB\texample", ex1, "Expected format: size\\tfilename")
+}
+
+func TestGetPathSizeNotHumanDirAll(t *testing.T) {//r
+    ex1, err1 := code1.GetPathSize("example", true, false, true)
+    if err1 != nil {
+        t.Errorf(`Error in testing of getting path size %v`, err1)
+    }
+    require.Equal(t, "19689B\texample", ex1, "Expected format: size\\tfilename")
+}
