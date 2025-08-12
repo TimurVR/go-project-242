288e4c695ef494b673a9de3774aa1b9efea5d8e7
TimurVR <138249026+TimurVR@users.noreply.github.com>
2025-08-12 19:32:41 +0300

    Try to fix problem in test

--- function.go
+++ function.go
@@ -15,7 +15,7 @@ func GetPathSize(path string, recursive, human, all bool) (string, error) {
 		totalSize = fileInfo.Size()
 	} else {
 		if all{
-		err = filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		    err = filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
 			if err != nil {
 				return err
 			}
